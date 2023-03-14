243dafb86ae8be87e6b3173d5a8947ccfc4a7b98
Genya10 <113167825+Genya10@users.noreply.github.com>
2023-03-14 11:29:50 +0200

    Add files via upload

---
+++ folder.js/script.js
@@ -0,0 +1,16 @@
+
+/*let link = document.querySelector('link[rel="import"]').import;
+console.log(link);
+alert('Hello,bro');
+let header = document.querySelector('.header');
+console.log(header);
+let logo = document.querySelector('.logo');
+console.log(logo);
+document.body.append(header.cloneNode(true));*/
+
+/*let btn_order = document.querySelectorAll('.btn_order');
+console.log(btn_order);
+let link = document.querySelectorAll('.link');
+console.log(link);
+let order = document.querySelector('.order');
+console.log(order);*/
